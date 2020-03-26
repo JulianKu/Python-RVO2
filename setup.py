9b5266df900796ca638afcbf85d0596d64b4a1e3
Julian Kunze <julian.kunze@tum.de>
2020-03-26 19:17:19 +0100

    Make package catkin compliant
    add package.xml
    modify CMakeLists.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 from Cython.Build import cythonize
+from catkin_pkg.python_setup import generate_distutils_setup
 
 
 class BuildRvo2Ext(_build_ext):
@@ -22,6 +25,13 @@ class BuildRvo2Ext(_build_ext):
         _build_ext.run(self)
 
 
+setup_args = generate_distutils_setup(
+    version='0.1.0',
+    name='pyrvo2',
+    maintainer='Julian Kunze',
+    maintainer_email='julian-kunze@gmx.de',
+)
+
 extensions = [
     Extension('rvo2', ['src/*.pyx'],
               include_dirs=['src'],
@@ -31,7 +41,6 @@ extensions = [
 ]
 
 setup(
-    name="pyrvo2",
     ext_modules=cythonize(extensions),
     cmdclass={'build_ext': BuildRvo2Ext},
     classifiers=[
@@ -48,4 +57,5 @@ setup(
         'Topic :: Games/Entertainment :: Simulation',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    **setup_args
 )
