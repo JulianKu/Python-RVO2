ebde1f00809badc1d0cfbc8e6d57e99737ddfd64
Julian Kunze <julian.kunze@tum.de>
2020-03-27 11:19:51 +0100

    Revert some configuration changes that caused compilation issues

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 from Cython.Build import cythonize
-from catkin_pkg.python_setup import generate_distutils_setup
 
 
 class BuildRvo2Ext(_build_ext):
@@ -25,13 +22,6 @@ class BuildRvo2Ext(_build_ext):
         _build_ext.run(self)
 
 
-setup_args = generate_distutils_setup(
-    version='0.1.0',
-    name='pyrvo2',
-    maintainer='Julian Kunze',
-    maintainer_email='julian-kunze@gmx.de',
-)
-
 extensions = [
     Extension('rvo2', ['src/*.pyx'],
               include_dirs=['src'],
@@ -41,6 +31,7 @@ extensions = [
 ]
 
 setup(
+    name="pyrvo2",
     ext_modules=cythonize(extensions),
     cmdclass={'build_ext': BuildRvo2Ext},
     classifiers=[
@@ -57,5 +48,4 @@ setup(
         'Topic :: Games/Entertainment :: Simulation',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    **setup_args
 )
